a0c79680d1296fe186c4dbd8c334959cb99dbe1e
Adrian <adrianp1447@gmail.com>
2022-04-04 17:27:29 -0400

    First steps off store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopifyProductDetailsComponent } from './components/shopify-product-details/shopify-product-details.component';
+import { ShopifyStoreComponent } from './components/shopify-store/shopify-store.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  //
+  { path: '', redirectTo: '/store', pathMatch: 'full' },
+  { path: 'product/:id', component: ShopifyProductDetailsComponent },
+  { path: 'store', component: ShopifyStoreComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
